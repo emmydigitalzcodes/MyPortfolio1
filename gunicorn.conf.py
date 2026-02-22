bb8f9b97a59ee9832fe57139364c2ccf4af41074
emmydigtalzcodes <moseschukwuemeka07@gmail.com>
2026-02-22 14:33:45 +0100

    rail

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -30,7 +30,7 @@ tmp_upload_dir = None
 
 # SSL (handled by Railway)
 forwarded_allow_ips = '*'
- secure_scheme_headers = {
+secure_scheme_headers = {
      'X-FORWARDED-PROTOCOL': 'ssl',
      'X-FORWARDED-PROTO': 'https',
      'X-FORWARDED-SSL': 'on'
