c09f4a46fe68d06c3f6d28afa57f38e5e7240208
emmydigtalzcodes <moseschukwuemeka07@gmail.com>
2026-02-22 14:36:20 +0100

    allowedhost

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key-change
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = [".railway.app"
+ALLOWED_HOSTS = [".railway.app", "web-production-5cb8c.up.railway.app"
 ]
 if DEBUG:
     ALLOWED_HOSTS += ['localhost', '127.0.0.1']
