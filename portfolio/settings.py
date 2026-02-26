4c4c927ad1af0875739cbffb23c032f54faca105
emmydigtalzcodes <moseschukwuemeka07@gmail.com>
2026-02-26 08:01:58 +0100

    sendgrid

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -170,19 +170,16 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Email Configuration (for contact form)
 # Use environment variable to control email backend
+# Email Configuration - SendGrid SMTP
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.sendgrid.net')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='apikey')  # This should be 'apikey'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Your SendGrid API key
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
-CONTACT_EMAIL = config('CONTACT_EMAIL', default=EMAIL_HOST_USER)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')  # Your verified sender
+CONTACT_EMAIL = config('CONTACT_EMAIL')  # Where you receive messages
 
-# Remove the console backend override - let EMAIL_BACKEND env variable control it
-# This way you can set:
-# - In development: EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend
-# - In production: EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
 
 # Security Settings (enable in production)
 if not DEBUG:
