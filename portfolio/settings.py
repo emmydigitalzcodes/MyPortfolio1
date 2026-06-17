000b7f457523843d56d54a55e59408747ab74595
emmydigtalzcodes <moseschukwuemeka07@gmail.com>
2026-06-17 15:09:57 +0100

    jazzmin

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = [
 # Application definition
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
